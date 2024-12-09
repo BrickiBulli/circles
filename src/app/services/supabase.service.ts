ac1c1b5b691b26260003495cbb2b6f56b34dc712
BrickiBulli <seth.schmutz@bbzbl-it.ch>
2024-12-09 16:14:44 +0100

    small change

--- src/app/services/supabase.service.ts
+++ src/app/services/supabase.service.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://your-project-url.supabase.co';
-const supabaseKey = 'your-anon-public-api-key';
+const supabaseUrl = 'https://eshglmqxleeljzmujkkk.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzaGdsbXF4bGVlbGp6bXVqa2trIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM3NTU3OTIsImV4cCI6MjA0OTMzMTc5Mn0.LAkAStxdV2QhBpUoMj3xG-mvJtoX0JjB1VCz9IwrDxU';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon, IonList, IonItem, IonLabel } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { supabase } from '../services/supabase.service'; 
+import { IonicModule } from '@ionic/angular'; // Import IonicModule
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonLabel, IonItem, IonList, IonIcon, IonButton, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonLabel, IonItem, IonList, IonIcon, IonButton, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, IonicModule, ExploreContainerComponent]
 })
 export class Tab2Page {
 
@@ -23,7 +24,7 @@ export class Tab2Page {
   // Function to load chatrooms from Supabase
   async loadChatrooms() {
     const { data, error } = await supabase
-      .from('chatrooms') // Your chatrooms table
+      .from('chatroom') // Your chatrooms table
       .select('*');
 
     if (error) {
